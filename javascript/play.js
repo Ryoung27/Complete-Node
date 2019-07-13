fda4b34fa2f2c3e4b8c9e9c4f61864471157557c
Ryoung27 <RRYoung89@gmail.com>
2019-07-13 13:16:30 -0500

    If I write another function

--- javascript/play.js
+++ javascript/play.js
@@ -1,10 +1,33 @@
-var name = 'Richie';
-var age = 29;
-var hasHobbies = true;
+const name = 'Richie';
+let age = 29;
+const hasHobbies = true;
 
-function summarizeUser(userName, userAge, userHasHobby){
+const summarizeUser = (userName, userAge, userHasHobby) => {
+
     return 'Name is ' + userName + ', age is ' + userAge + ' has hobbies ' + userHasHobby;
 }
 
+// const add = (a,b) => {
+//     return a + b;
+// }
+
+const add = (a,b) => a +b;
+
+console.log(add(1,2));
+
 console.log(summarizeUser(name, age, hasHobbies))
 console.log(summarizeUser('Ronnie', '30', false))
+
+const person = {
+    name: 'Richie',
+    age: 29,
+    greet(){
+        console.log("Hi, I am " + this.name)
+    }
+}
+
+console.log(person)
+console.log(person.name)
+console.log(person.age)
+person.greet()
+
