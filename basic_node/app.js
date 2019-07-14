a3f308c8a623c58e5065c17d34afba0029f91b50
Ryoung27 <RRYoung89@gmail.com>
2019-07-14 12:36:40 -0500

    the most basic of webservers

---
+++ basic_node/app.js
@@ -0,0 +1,18 @@
+//Creating a server.
+// Importing http from global Node.
+const http = require('http');
+
+
+//Using the create server method.
+//Takes a request listener as a constant
+//Takes request and response
+// function rqListener(req, res){
+
+// }
+//Event driven.
+const server = http.createServer((req, res) =>{
+    console.log(req);
+});
+
+//Listen for incoming request. Defaults to port 80.
+server.listen(3000);
