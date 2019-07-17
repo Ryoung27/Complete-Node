2a4955e0eeee24da55c7ec48ccd0b37f17cffc1e
Ryoung27 <RRYoung89@gmail.com>
2019-07-16 19:34:17 -0500

    redirecting request

--- basic_node/app.js
+++ basic_node/app.js
@@ -1,6 +1,7 @@
 //Creating a server.
 // Importing http from global Node.
 const http = require('http');
+const fs = require('fs');
 
 
 //Using the create server method.
@@ -14,11 +15,19 @@ const server = http.createServer((req, res) =>{
     const url = req.url;
     //Process exit is a hard exit of code.
     // process.exit();
+    const method = req.method;
     if (url === '/'){
         res.write('<html>');
         res.write('<head><title>Enter message</title></head>')
         res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></body>');
         res.write('</html>');
+        return res.end();
+    }
+
+    if (url === '/message' && method === 'POST'){
+        fs.writeFileSync('message.txt', 'DUMMY');
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
         return res.end();
     }
     res.setHeader('Content-Type', 'text/html')
