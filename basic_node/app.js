94ef9e776ea6a28b0344032ed2922636f95a90aa
Ryoung27 <RRYoung89@gmail.com>
2019-07-19 08:50:44 -0500

    express

--- basic_node/app.js
+++ basic_node/app.js
@@ -1,21 +1,11 @@
-//Creating a server.
-// Importing http from global Node.
 const http = require('http');
-const routes = require('./routes');
+
+const express = require('express');
 
 
-//Using the create server method.
-//Takes a request listener as a constant
-//Takes request and response
-// function rqListener(req, res){
+const app = express();
 
-// }
-//Event driven.
+const server = http.createServer(app);
 
-console.log(routes.someText);
-
-const server = http.createServer(routes.handler);
-
-//Listen for incoming request. Defaults to port 80.
 server.listen(3000);
 
