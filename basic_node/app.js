18c5979baaab2895148dfc3237eff9bb1397713a
Ryoung27 <RRYoung89@gmail.com>
2019-07-20 15:11:03 -0500

    middle where?

--- basic_node/app.js
+++ basic_node/app.js
@@ -5,6 +5,15 @@ const express = require('express');
 
 const app = express();
 
+app.use((req, res, next) => {
+    console.log('In middleware.');
+    next();
+});
+
+app.use((req, res, next) => {
+    console.log('In another middleware!');
+});
+
 const server = http.createServer(app);
 
 server.listen(3000);
