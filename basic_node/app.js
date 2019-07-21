c4642aa78b9304736d01c9bcdd88b8afb86bc2aa
Ryoung27 <RRYoung89@gmail.com>
2019-07-21 08:06:24 -0500

    routers

--- basic_node/app.js
+++ basic_node/app.js
@@ -1,23 +1,16 @@
 const express = require('express');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
 
 const app = express();
 
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
 app.use(bodyParser.urlencoded({extended: false}));
 
+app.use(adminRoutes);
+app.use(shopRoutes);
 
-app.use('/add-product', (req, res, next) => {
-    res.send('<form action="/product" method="POST"><input type="text" name = "title"><button type="submit">Add product</button></form>');
-});
-
-app.post('/product', (req, res, next) =>{
-    console.log(req.body);
-    res.redirect('/');
-});
-
-app.use('/', (req, res, next) => {
-    res.send('<h1>Express</h1>');
-});
 
 
 app.listen(3000);
