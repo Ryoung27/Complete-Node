1da237d3e0a8341fac8f7ea8902301513dfb7554
Ryoung27 <RRYoung89@gmail.com>
2019-07-21 10:43:23 -0500

    added 404 to standard file function

--- basic_node/app.js
+++ basic_node/app.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
 
@@ -8,8 +10,12 @@ const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.use(adminRoutes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
+
+app.use((req, res) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
+});
 
 
 
